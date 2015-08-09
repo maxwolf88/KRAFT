478077ed4c6518e98a05d5bfe3b9607d85357b5e
Max <maxwolf88@gmail.com>
2015-08-10 00:23:51 +0400

    dobavil biblioteku jQuery i HYI, ne rabotaet selectmenu

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $("#owl-banner").owlCarousel(
+
+        {navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true}
+    );
+
+    $( "#menu" ).selectmenu();
+});
+
+
+
+
+
+
